c56110a8b3fe8363c4f5dac8fba57c362384432d
raxden <raxden.dev@gmail.com>
2017-06-09 19:00:40 +0200

    Release 3.0.28 created

---
+++ sample/src/main/java/com/raxdenstudios/square/sample/SampleActivity.java
@@ -0,0 +1,32 @@
+package com.raxdenstudios.square.sample;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.raxdenstudios.square.SquareActivity;
+import com.raxdenstudios.square.interceptor.ActivityInterceptor;
+import com.raxdenstudios.square.interceptor.autoinflatelayout.AutoInflateLayoutActivityInterceptor;
+import com.raxdenstudios.square.interceptor.autoinflatelayout.AutoInflateLayoutInteractor;
+import com.raxdenstudios.square.interceptor.autoinflatelayout.AutoInflateLayoutInterceptorCallback;
+
+import java.util.List;
+
+public class SampleActivity extends SquareActivity {
+
+    @Override
+    protected void addInterceptor(List<ActivityInterceptor> interceptors) {
+        interceptors.add(
+                new AutoInflateLayoutActivityInterceptor(this, new AutoInflateLayoutInterceptorCallback() {
+                    @Override
+                    public void onContentViewCreated(View view, Bundle savedInstanceState) {
+
+                    }
+
+                    @Override
+                    public void onInterceptorAttached(AutoInflateLayoutInteractor interactor) {
+
+                    }
+                }));
+    }
+
+}
