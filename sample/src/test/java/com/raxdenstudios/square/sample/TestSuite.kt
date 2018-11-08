39eb2c3da4c67182635b0ee21aa30860901233da
Ángel Gómez <raxden.dev@gmail.com>
2018-11-08 17:50:42 +0100

    add new test

--- sample/src/test/java/com/raxdenstudios/square/sample/TestSuite.kt
+++ sample/src/test/java/com/raxdenstudios/square/sample/TestSuite.kt
@@ -2,6 +2,7 @@ package com.raxdenstudios.square.sample
 
 
 import com.raxdenstudios.square.sample.commons.AutoInflateLayoutActivityTest
+import com.raxdenstudios.square.sample.commons.FragmentStatePagerActivityTest
 import com.raxdenstudios.square.sample.commons.ToolbarActivityTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
@@ -9,6 +10,7 @@ import org.junit.runners.Suite
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
         AutoInflateLayoutActivityTest::class,
+        FragmentStatePagerActivityTest::class,
         ToolbarActivityTest::class
 )
 class TestSuite
