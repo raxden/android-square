cbd9487a14b4e28483260d8962c290d98da5ecaf
Ángel Gómez <raxden.dev@gmail.com>
2019-02-02 11:27:57 +0100

    update

---
+++ square-commons/src/main/java/com/raxdenstudios/square/interceptor/commons/inflateview/InflateViewInterceptor.kt
@@ -0,0 +1,13 @@
+package com.raxdenstudios.square.interceptor.commons.inflateview
+
+
+import com.raxdenstudios.square.interceptor.Interceptor
+
+/**
+ * Created by agomez on 02/06/2015.
+ */
+interface InflateViewInterceptor : Interceptor {
+
+    fun setLayoutId(layoutId: Int)
+
+}
